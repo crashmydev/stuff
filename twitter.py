4f2ff5e7164e031a63a6df5b98a83070a64a36b6
Tim Hollinger <tim-hollinger@t-online.de>
2018-07-08 15:39:17 +0200

    new model approach

--- twitter.py
+++ twitter.py
@@ -1,14 +1,60 @@
-import pandas as ps
-from keras.preprocessing import sequence
-from keras.models import Sequential
-from keras.layers import Dense, Embedding
-from keras.layers import LSTM, GRU
-from keras.optimizers import Adam
+import numpy as np # linear algebra
+import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
+
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.feature_extraction.text import TfidfTransformer
+from keras.preprocessing.text import Tokenizer
+from keras.preprocessing.sequence import pad_sequences
+from keras.models import Sequential
+from keras.layers import Dense, Embedding, LSTM, SpatialDropout1D
+from sklearn.model_selection import train_test_split
+from keras.utils.np_utils import to_categorical
+import re
+
+data = pd.read_csv('./Airline.csv')
+# Keeping only the neccessary columns
+data = data[['text','airline_sentiment']]
+
+data = data[data.airline_sentiment != "neutral"]
+data['text'] = data['text'].apply(lambda x: x.lower())
+data['text'] = data['text'].apply((lambda x: re.sub('[^a-zA-z0-9\s]', '', x)))
+
+print(data[data['airline_sentiment'] == 'positive'].size)
+print(data[data['airline_sentiment'] == 'negative'].size)
+
+for idx, row in data.iterrows():
+    row[0] = row[0].replace('rt', ' ')
+
+max_fatures = 2000
+tokenizer = Tokenizer(num_words=max_fatures, split=' ')
+tokenizer.fit_on_texts(data['text'].values)
+X = tokenizer.texts_to_sequences(data['text'].values)
+X = pad_sequences(X)
 
 
-dataset = ps.read_csv("tweet_product_company.csv", sep = ',')
-count_vec = CountVectorizer()
-data = count_vec.fit_transform(dataset)
-print count_vec.get_feature_names()
+embed_dim = 128
+lstm_out = 196
+
+model = Sequential()
+model.add(Embedding(max_fatures, embed_dim,input_length = X.shape[1]))
+model.add(SpatialDropout1D(0.4))
+model.add(LSTM(lstm_out, dropout=0.2, recurrent_dropout=0.2))
+model.add(Dense(2,activation='softmax'))
+model.compile(loss = 'categorical_crossentropy', optimizer='adam',metrics = ['accuracy'])
+print(model.summary())
+
+Y = pd.get_dummies(data['airline_sentiment']).values
+X_train, X_test, Y_train, Y_test = train_test_split(X,Y, test_size = 0.33, random_state = 42)
+print(X_train.shape,Y_train.shape)
+print(X_test.shape,Y_test.shape)
+
+batch_size = 32
+model.fit(X_train, Y_train, epochs = 7, batch_size=batch_size, verbose = 2)
+validation_size = 1500
+
+X_validate = X_test[-validation_size:]
+Y_validate = Y_test[-validation_size:]
+X_test = X_test[:-validation_size]
+Y_test = Y_test[:-validation_size]
+score,acc = model.evaluate(X_test, Y_test, verbose = 2, batch_size = batch_size)
+print("score: %.2f" % (score))
+print("acc: %.2f" % (acc))
